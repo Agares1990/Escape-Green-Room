c4b99a8b7c066fbce33a582898f89732420db4b0
Brahim <lamjarad@gmail.com>
2023-04-15 22:45:57 +0200

    Nouvelle Scene avec voiture

--- js/main.js
+++ js/main.js
@@ -1,215 +1,252 @@
+let tunnel;
+let car;
 let canvas;
 let engine;
 let scene;
 // vars for handling inputs
 let inputStates = {};
+let followCamera;
+let score = 0;
+async function startGame() {
+  canvas = document.querySelector("#myCanvas");
+  engine = new BABYLON.Engine(canvas, true);
+  scene = await createScene();
+  var sound = new BABYLON.Sound("music", "assets/music/DRIVE.mp3", scene, null, {
+    loop: true,
+    autoplay: true
+});
 
-window.onload = startGame;
+  modifySettings();
+  // run the render loop
+  // render : resituer
 
-function startGame() {
-    canvas = document.querySelector("#myCanvas");
-    engine = new BABYLON.Engine(canvas, true);
-    scene = createScene();
-
-    // modify some default settings (i.e pointer events to prevent cursor to go 
-    // out of the game window)
-    modifySettings();
-
-    let Car = scene.getMeshByName("Car");
-
-    engine.runRenderLoop(() => {
-        let deltaTime = engine.getDeltaTime(); // remind you something ?
-
-        Car.move();
-        scene.render();
-    });
+  engine.runRenderLoop(() => {
+    let deltaTime = engine.getDeltaTime(); 
+    car.move();
+    sound.loop = true;
+    scene.render();
+  });
 }
 
-function createScene() {
-    let scene = new BABYLON.Scene(engine);
-    let ground = createGround(scene);
-    let freeCamera = createFreeCamera(scene);
-
-    let Car = createCar(scene);
-
-    // second parameter is the target to follow
-    let followCamera = createFollowCamera(scene, Car);
+//create a scene
+async function createScene() {
+  scene = new BABYLON.Scene(engine);
+  createLight(scene);
+  //skybox2(scene);
+  createGroundFromGLB(scene);
+  tunnel = await createGroundFromGLB(scene);
+  car = await createCar(scene);
+  console.log(car);
+  if (car) {
+    followCamera = createFollowCamera(scene, car);
     scene.activeCamera = followCamera;
+    
+  }
 
-    createLights(scene);
-   return scene;
+
+  return scene;
+}
+//sky from image
+function skybox2(scene) {
+  var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+  var skyboxMaterial = new BABYLON.StandardMaterial("skyBoxMaterial", scene);
+  skyboxMaterial.backFaceCulling = false;
+  skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture(
+    "assets/images/cubemap/",
+    scene
+  );
+  skyboxMaterial.reflectionTexture.coordinatesMode =
+    BABYLON.Texture.SKYBOX_MODE;
+  skyboxMaterial.disableLighting = true;
+  skybox.material = skyboxMaterial;
 }
 
-function createGround(scene) {
-    const groundOptions = { width:70, height:20000, subdivisions:20, minHeight:0, maxHeight:0, onReady: onGroundCreated};
-    //scene is optional and defaults to the current scene
-    const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("gdhm", 'images/hmap1.png', groundOptions, scene); 
+function createGroundFromGLB(scene) {
+  return BABYLON.SceneLoader.ImportMeshAsync("", "assets/models/", "road.glb", scene).then((result) => {
+    let ground = result.meshes[0];
+    ground.position = new BABYLON.Vector3(0, 0, 0);
+    ground.scaling = new BABYLON.Vector3(4, 4, 4);
+    ground.rotation = new BABYLON.Vector3(0, 0, 0);
+    ground.checkCollisions = true;
+    ground.receiveShadows = true;
 
-    function onGroundCreated() {
-        const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-        groundMaterial.diffuseTexture = new BABYLON.Texture("images/road.png");
-        ground.material = groundMaterial;
-        // to be taken into account by collision detection
-        ground.checkCollisions = true;
-        //groundMaterial.wireframe=true;
-    }
     return ground;
+  });
 }
 
-function createLights(scene) {
-    // i.e sun light with all light rays parallels, the vector is the direction.
-    let light0 = new BABYLON.DirectionalLight("dir0", new BABYLON.Vector3(-1, -1, 0), scene);
+// Call the function to create the tunnel mesh
 
-}
 
-function createFreeCamera(scene) {
-    let camera = new BABYLON.FreeCamera("freeCamera", new BABYLON.Vector3(0, 50, 0), scene);
-    camera.attachControl(canvas);
-    // prevent camera to cross ground
-    camera.checkCollisions = true; 
-    // avoid flying with the camera
-    camera.applyGravity = true;
-
-    // Add extra keys for camera movements
-    // Need the ascii code of the extra key(s). We use a string method here to get the ascii code
-    camera.keysUp.push('z'.charCodeAt(0));
-    camera.keysDown.push('s'.charCodeAt(0));
-    camera.keysLeft.push('q'.charCodeAt(0));
-    camera.keysRight.push('d'.charCodeAt(0));
-    camera.keysUp.push('Z'.charCodeAt(0));
-    camera.keysDown.push('S'.charCodeAt(0));
-    camera.keysLeft.push('Q'.charCodeAt(0));
-    camera.keysRight.push('D'.charCodeAt(0));
-
-    return camera;
-}
-
+//create a follow camera
 function createFollowCamera(scene, target) {
-    let camera = new BABYLON.FollowCamera("CarFollowCamera", target.position, scene, target);
+    let camera = new BABYLON.FollowCamera("carFollowCamera", target.position, scene, target);
 
-    camera.radius = 15; // how far from the object to follow
-	camera.heightOffset = 2; // how high above the object to place the camera
+    camera.radius = 30; // how far from the object to follow
+	camera.heightOffset = 8; // how high above the object to place the camera
 	camera.rotationOffset = 180; // the viewing angle
 	camera.cameraAcceleration = .1; // how fast to move
 	camera.maxCameraSpeed = 5; // speed limit
 
     return camera;
 }
-
+// create a light
+//up on the sky and light up the scene / from above
+function createLight(scene) {
+  const light = new BABYLON.HemisphericLight(
+    "HemiLight",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
+}
 let zMovement = 5;
-function createCar(scene) {
-    let Car = BABYLON.SceneLoader.ImportMesh("", "/models/", "car.glb", scene, function(){
-    // let CarMaterial = new BABYLON.StandardMaterial("CarMaterial", scene);
-    // CarMaterial.diffuseColor = new BABYLON.Color3.Red;
-    // CarMaterial.emissiveColor = new BABYLON.Color3.Blue;
-    // Car.material = CarMaterial;
+//creating a car
+async function createCar(scene) {
+  let car = await BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "assets/models/",
+    "old_rusty_car.glb",
+    scene
+  );
 
-    // By default the box/Car is in 0, 0, 0, let's change that...
-    Car.position.y = 0.6;
-    Car.speed = 1;
-    Car.frontVector = new BABYLON.Vector3(0, 0, 1);
 
-    Car.move = () => {
-                //Car.position.z += -1; // speed should be in unit/s, and depends on
-                                 // deltaTime !
 
-        // if we want to move while taking into account collision detections
-        // collision uses by default "ellipsoids"
 
-        let yMovement = 0;
-       
-        if (Car.position.y > 2) {
-            zMovement = 0;
-            yMovement = -2;
-        } 
-        //Car.moveWithCollisions(new BABYLON.Vector3(0, yMovement, zMovement));
-
-        
-        if(inputStates.up) {
-            //Car.moveWithCollisions(new BABYLON.Vector3(0, 0, 1*Car.speed));
-            Car.moveWithCollisions(Car.frontVector.multiplyByFloats(Car.speed, Car.speed, Car.speed));
-        }    
-        if(inputStates.down) {
-            //Car.moveWithCollisions(new BABYLON.Vector3(0, 0, -1*Car.speed));
-            Car.moveWithCollisions(Car.frontVector.multiplyByFloats(-Car.speed, -Car.speed, -Car.speed));
-
-        }  
-        if(inputStates.left) {
-            //Car.moveWithCollisions(new BABYLON.Vector3(-1*Car.speed, 0, 0));
-            Car.rotation.y -= 0.02;
-            Car.frontVector = new BABYLON.Vector3(Math.sin(Car.rotation.y), 0, Math.cos(Car.rotation.y));
-        }    
-        if(inputStates.right) {
-            //Car.moveWithCollisions(new BABYLON.Vector3(1*Car.speed, 0, 0));
-            Car.rotation.y += 0.02;
-            Car.frontVector = new BABYLON.Vector3(Math.sin(Car.rotation.y), 0, Math.cos(Car.rotation.y));
-        }
-    }
+  if (car.meshes.length > 0) {
     
-    return Car;
-})};
+    car.meshes[0].position = new BABYLON.Vector3(0, 15,750);
+    car.meshes[0].scaling = new BABYLON.Vector3(0.015, 0.015, 0.015);
+    car.meshes[0].frontVector = new BABYLON.Vector3(0, 0, 1);
+    car.meshes[0].speed = 1.8;
+     car.meshes[0].move = () => { 
+      let carActionManager = new BABYLON.ActionManager(scene);
 
-window.addEventListener("resize", () => {
-    engine.resize()
-});
-
-function modifySettings() {
-    // as soon as we click on the game window, the mouse pointer is "locked"
-    // you will have to press ESC to unlock it
-    scene.onPointerDown = () => {
-        if(!scene.alreadyLocked) {
-            console.log("requesting pointer lock");
-            canvas.requestPointerLock();
-        } else {
-            console.log("Pointer already locked");
-        }
-    }
-
-    document.addEventListener("pointerlockchange", () => {
-        let element = document.pointerLockElement || null;
-        if(element) {
-            // lets create a custom attribute
-            scene.alreadyLocked = true;
-        } else {
-            scene.alreadyLocked = false;
-        }
-    })
-
-    // key listeners for the Car
-    inputStates.left = false;
-    inputStates.right = false;
-    inputStates.up = false;
-    inputStates.down = false;
-    inputStates.space = false;
+      // register an action to be triggered when the car collides with a fish
+      carActionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+          {
+            trigger: BABYLON.ActionManager.OnIntersectionEnterTrigger,
+            parameter: { mesh: car.meshes[0], usePreciseIntersection: true }
+          },
+        )
+      ); 
     
-    //add the listener to the main, window object, and update the states
-    window.addEventListener('keydown', (event) => {
-        if ((event.key === "ArrowLeft") || (event.key === "q")|| (event.key === "Q")) {
-           inputStates.left = true;
-        } else if ((event.key === "ArrowUp") || (event.key === "z")|| (event.key === "Z")){
-           inputStates.up = true;
-        } else if ((event.key === "ArrowRight") || (event.key === "d")|| (event.key === "D")){
-           inputStates.right = true;
-        } else if ((event.key === "ArrowDown")|| (event.key === "s")|| (event.key === "S")) {
-           inputStates.down = true;
-        }  else if (event.key === " ") {
-           inputStates.space = true;
-        }
-    }, false);
+      let yMovement = 0;
+      if (car.meshes[0].position.y > 2) {
+        zMovement = 0;
+        yMovement = -2;
+      }
 
-    //if the key will be released, change the states object 
-    window.addEventListener('keyup', (event) => {
-        if ((event.key === "ArrowLeft") || (event.key === "q")|| (event.key === "Q")) {
-           inputStates.left = false;
-        } else if ((event.key === "ArrowUp") || (event.key === "z")|| (event.key === "Z")){
-           inputStates.up = false;
-        } else if ((event.key === "ArrowRight") || (event.key === "d")|| (event.key === "D")){
-           inputStates.right = false;
-        } else if ((event.key === "ArrowDown")|| (event.key === "s")|| (event.key === "S")) {
-           inputStates.down = false;
-        }  else if (event.key === " ") {
-           inputStates.space = false;
-        }
-    }, false);
+      if (inputStates.up) {
+        car.meshes[0].moveWithCollisions(new BABYLON.Vector3(0, 0, -1 * car.meshes[0].speed));
+      } 
+      if (inputStates.down) {
+        car.meshes[0].moveWithCollisions(new BABYLON.Vector3(0, 0, 1 * car.meshes[0].speed));
+      }
+      if (inputStates.left) {
+        car.meshes[0].moveWithCollisions(new BABYLON.Vector3(1 * car.meshes[0].speed, 0, 0));
+      }
+      if (inputStates.right) {
+        car.meshes[0].moveWithCollisions(new BABYLON.Vector3(-1 * car.meshes[0].speed, 0, 0));
+      }
+    }
+    return car.meshes[0];
+  } else {
+    console.error("No meshes found in car.glb");
+    return null;
+  }
 }
 
+
+function modifySettings() {
+  // as soon as we click on the game window, the mouse pointer is "locked"
+  // you will have to press ESC to unlock it
+  scene.onPointerDown = () => {
+    if (!scene.alreadyLocked) {
+      console.log("requesting pointer lock");
+      canvas.requestPointerLock();
+    } else {
+      console.log("Pointer already locked");
+    }
+  };
+
+  document.addEventListener("pointerlockchange", () => {
+    let element = document.pointerLockElement || null;
+    if (element) {
+      // lets create a custom attribute
+      scene.alreadyLocked = true;
+    } else {
+      scene.alreadyLocked = false;
+    }
+  });
+
+  // key listeners for the car.meshes[0]
+  inputStates.left = false;
+  inputStates.right = false;
+  inputStates.up = false;
+  inputStates.down = false;
+  inputStates.space = false;
+
+  //add the listener to the main, window object, and update the states
+  window.addEventListener(
+    "keydown",
+    (event) => {
+      if (event.key === "ArrowLeft" || event.key === "q" || event.key === "Q") {
+        inputStates.left = true;
+      } else if (
+        event.key === "ArrowUp" ||
+        event.key === "z" ||
+        event.key === "Z"
+      ) {
+        inputStates.up = true;
+      } else if (
+        event.key === "ArrowRight" ||
+        event.key === "d" ||
+        event.key === "D"
+      ) {
+        inputStates.right = true;
+      } else if (
+        event.key === "ArrowDown" ||
+        event.key === "s" ||
+        event.key === "S"
+      ) {
+        inputStates.down = true;
+      } else if (event.key === " ") {
+        inputStates.space = true;
+      }
+    },
+    false
+  );
+
+  //if the key will be released, change the states object
+  window.addEventListener(
+    "keyup",
+    (event) => {
+      if (event.key === "ArrowLeft" || event.key === "q" || event.key === "Q") {
+        inputStates.left = false;
+      } else if (
+        event.key === "ArrowUp" ||
+        event.key === "z" ||
+        event.key === "Z"
+      ) {
+        inputStates.up = false;
+      } else if (
+        event.key === "ArrowRight" ||
+        event.key === "d" ||
+        event.key === "D"
+      ) {
+        inputStates.right = false;
+      } else if (
+        event.key === "ArrowDown" ||
+        event.key === "s" ||
+        event.key === "S"
+      ) {
+        inputStates.down = false;
+      } else if (event.key === " ") {
+        inputStates.space = false;
+      }
+    },
+    false
+  );
+}
+
+window.onload = startGame;
